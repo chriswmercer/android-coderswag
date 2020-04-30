cc93004000118ec9f812ab8f4ae1ac34a8c6b30f
Unknown <chriswm@me.com>
2020-04-30 18:18:19 +0100

    Lesson 3 - Simple ListView

--- coderswag/app/src/main/java/dev/chrismercer/coderswag/controller/MainActivity.kt
+++ coderswag/app/src/main/java/dev/chrismercer/coderswag/controller/MainActivity.kt
@@ -2,12 +2,21 @@ package dev.chrismercer.coderswag.controller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import dev.chrismercer.coderswag.R
+import dev.chrismercer.coderswag.model.Category
+import dev.chrismercer.coderswag.services.DataService
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var adapter : ArrayAdapter<Category>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, DataService.Categories)
+        categoryListView.adapter = adapter
     }
 }
