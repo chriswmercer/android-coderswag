4557031049d5b6d6ab7505bea32dff6b63ab0889
Unknown <chriswm@me.com>
2020-04-30 17:28:50 +0100

    Lesson 2 - Data structures

---
+++ coderswag/app/src/main/java/dev/chrismercer/coderswag/controller/MainActivity.kt
@@ -0,0 +1,13 @@
+package dev.chrismercer.coderswag.controller
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import dev.chrismercer.coderswag.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ coderswag/app/src/main/java/dev/chrismercer/coderswag/model/Product.kt
@@ -0,0 +1,3 @@
+package dev.chrismercer.coderswag.model
+
+class Product(val title: String, val price: String, val image: String)
